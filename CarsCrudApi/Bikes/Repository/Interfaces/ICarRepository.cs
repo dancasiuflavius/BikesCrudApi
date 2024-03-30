04b54533b2ecba2e18cba72916f7f5a493b9be1a
Dancasiu Flavius <dancasiu.flavius@yahoo.com>
2024-03-30 17:51:37 +0200

    Commit

---
+++ CarsCrudApi/Bikes/Repository/Interfaces/ICarRepository.cs
@@ -0,0 +1,17 @@
+﻿using CarsCrudApi.Cars.Dto;
+using CarsCrudApi.Cars.Model;
+
+namespace CarsCrudApi.Cars.Repository.Interfaces
+{
+    public interface ICarRepository
+    {
+        Task<IEnumerable<Bike>> GetAllAsync();
+        Task<Bike> GetByNameAsync(string name);
+
+        Task<IEnumerable<Double>> GetAllAsyncPrice();
+        //Task<Product> GetByIdAsync(int id);
+        Task<Bike> CreateAsync(CreateCarRequest carRequest);
+        //Task<Product> UpdateAsync(int id, UpdateProductRequest productRequest);
+        //Task DeleteAsync(int id);
+    }
+}
---
+++ CarsCrudApi/Data/data.sql
@@ -0,0 +1,7 @@
+﻿insert into cars (id, price, name, category, date_of_fabrication) values (1, 220.25, 'Yamaha R1', 'Circuit', '2012-02-04');
+insert into cars (id, price, name, category, date_of_fabrication) values (2, 576.08, 'Yamaha R6', 'SSport', '2010-09-17');
+insert into cars (id, price, name, category, date_of_fabrication) values (3, 630.78, 'Suzuki GSXR', 'SSport', '2011-05-25');
+insert into cars (id, price, name, category, date_of_fabrication) values (4, 536.19, 'Yamaha R3', 'Sport', '2019-05-30');
+insert into cars (id, price, name, category, date_of_fabrication) values (5, 947.3, 'Honda CBR', 'Sport', '2015-07-28');
+insert into cars (id, price, name, category, date_of_fabrication) values (6, 827.09, 'Ducati V4', 'Super sport', '2018-09-16');
+insert into cars (id, price, name, category, date_of_fabrication) values (7, 2.93, 'Harley', 'Chopper', '2021-01-10');
